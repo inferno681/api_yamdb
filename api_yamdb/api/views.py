065b98344d5ca3de8726f24af46def19045c5913
Василий Стакроцкий <inferno681@yandex.ru>
2023-11-09 19:48:59 +0100

    review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -83,6 +83,15 @@ class ReviewViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
     http_method_names = ('get', 'post', 'patch', 'delete')
 
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title
+
+    def get_queryset(self):
+        queryset = self.get_title().reviews.all()
+        return queryset
+
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
