822811e0a2d716475f164e7a663880ef1883d627
Kirill Konovalov <ki_ko11@mail.ru>
2023-11-10 00:53:44 +0300

    Add minor fix

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -93,15 +93,12 @@ class ReviewViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        if Review.objects.filter(
-                author=self.request.user,
-                title=title,
+        title = self.get_title()
+        if title.reviews.filter(
+                author=self.request.user
         ).exists():
             raise ValidationError(
                 {'review': ['You are already review this title.']})
-
         serializer.save(author=self.request.user, title=title)
         title.rating = mean(review.score for review in title.reviews.all())
         title.save()
