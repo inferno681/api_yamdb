14b4949877cfd2fdc389eb7f9c1e86b15f216830
Василий Стакроцкий <inferno681@gmail.com>
2023-11-10 23:25:25 +0100

    permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,30 +1,24 @@
 from rest_framework import permissions
 
-CONTENT_ACCESS = 'Изменение чужого контента запрещено!'
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return bool(request.method in permissions.SAFE_METHODS
-                    or request.user.is_authenticated
-                    and (request.user.is_admin
-                         or request.user.is_superuser))
-
 
 class IsAdminOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_admin or request.user.is_superuser
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin or request.user.is_superuser
+
+class IsAdminOrReadOnly(IsAdminOnly):
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                and super().has_permission(request, view))
 
 
 class IsAuthorOrStuffOrReadOnly(permissions.BasePermission):
+    message = 'Изменение чужого контента запрещено!'
 
     def has_object_permission(self, request, view, obj):
-        self.message = CONTENT_ACCESS
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_admin
