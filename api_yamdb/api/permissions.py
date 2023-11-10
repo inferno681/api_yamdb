4ee74f3cc9b777c78d189f09c99d11527f5f0f32
Василий Стакроцкий <inferno681@gmail.com>
2023-11-10 03:26:23 +0100

    all tests done

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,7 +17,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
         return bool(obj.author == request.user
                     or request.user.is_admin
-                    or request.user.role == 'moderator')
+                    or request.user.role == 'moderator'
+                    or request.user.is_superuser)
 
 
 class IsAdminOnly(permissions.BasePermission):
@@ -36,4 +37,5 @@ class IsAuthorOrStuffOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_admin
-                or request.user.is_moderator)
+                or request.user.is_moderator
+                or request.user.is_superuser)
